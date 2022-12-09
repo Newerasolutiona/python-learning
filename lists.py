330557296a05ee1840096b63a704bb9ff28029da
bilalasghar931 <bilalasghar931@gmail.com>
2022-12-09 07:01:36 +0500

    Adding python files

---
+++ lists.py
@@ -0,0 +1,12 @@
+my_list = ["January", "February", "March"]
+print(my_list)
+
+my_list.append("April")
+my_list.append("May")
+print(my_list)
+
+my_list.remove("February")
+print(my_list)
+
+my_list.append("May")
+print(my_list)
